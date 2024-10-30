f8394387f278cf09ca5a4fbfc8ba458920601869
Alex Silver <asilver@hmc.edu>
2024-10-30 14:19:14 -0700

    created draft of basic python file

---
+++ merged_notebooks_to_py.py
@@ -0,0 +1,224 @@
+######## ALEX CODE ##########
+
+import csv
+
+numTechElectives = 3 # how many tech electives are required?
+majorCSVpath = "/content/CS_Major_Requirements"
+
+
+def csvToLoL(csvpath):
+  """ input: file path to a CSV of course requirements, CSV formatted as shown below
+              major	techel	coursecode	sublist
+              CSCI  n       CSCI070     2
+              CSCI  y       MATH164     3
+
+      output: a list of lists, such that one course in each sublist must be taken
+            example: [["CSCI042, "CSCI060"], ["CSCI070]]]
+            indicates requirement of (CS 42 OR CS 60) AND CS 70
+  """
+  reqs = []
+  with open(csvpath) as f:
+    reader = csv.reader(f)
+    next(reader)
+    i=0
+    nonTechElectives = []
+    techElectives = []
+    for row in reader:
+      coursecode = row[2]
+      if row[1] == "y": # tech elective case
+        techElectives.append(coursecode)
+      else:
+        if int(row[-1])==i: # our CSV data is a string, and "0" =/= 0
+          nonTechElectives.append(coursecode)
+        else:
+          reqs.append(nonTechElectives)
+          nonTechElectives = []
+          nonTechElectives.append(coursecode)
+          i += 1
+    for i in range(numTechElectives):
+      reqs.append(techElectives) # deal with Tech Electives last
+    return reqs
+
+majorReqs = csvToLoL(majorCSVpath)
+
+coursesToDo = []
+coursesDone = []
+
+inputCourses = [
+    "CSCI005",
+    "CSCI060",
+    "MATH019"
+]
+
+def checkReqsMet(inputList, reqsList):
+    # inputList is a flat list
+    # reqsList is a list of lists
+    for subset in reqsList:
+        met = any(course in subset for course in inputList)
+        if met:
+            coursesDone.append(subset)
+        else:
+            coursesToDo.append(subset)
+
+coursesToDo = [] # emptied in case we rerun this cell
+coursesDone = [] # emptied in case we rerun this cell
+checkReqsMet(inputCourses, majorReqs)
+
+print("Courses Done is", coursesDone)
+print("Courses ToDo is", coursesToDo)
+
+###############
+###### NINA CODE ######
+
+# filtering by course area returns a list of course codes.
+import json
+
+def filterByArea(area):
+  with open("course-area.json", 'r') as file: # Open the JSON file
+      course_data = json.load(file)  # Load the JSON data from the file into list
+
+  output = []
+  for course in course_data:  # Print all courses that have the attribute of area
+    if area in set(course["course_areas"]):
+      courseOnly = course["course_code"][:-2].strip() # Remove two-lettered campus signature, remove whitespaces
+      output.append(courseOnly) # add course into final list
+  return output # return list of all courses in parameter area.
+
+
+filterByArea("CSCI")
+
+
+##### BRIGITTE CODE ####
+
+# filtering by course area returns a list of course codes.
+import json
+
+# at some point deal with CSV import of data but not yet!
+
+coursesToDo = []
+coursesDone  = []
+
+
+#example
+inputCourses = [
+    "CSCI005",
+    "CSCI060",
+    "MATH019",
+    "WRIT001",
+    "PHYS023",
+]
+
+#just an example, not extensive
+majorReqs = [
+    ["CSCI042", "CSCI060"],
+    ["CSCI070"],
+    ["CSCI081"],
+]
+
+#just an example, not extensive
+coreReqs = [
+    ["MATH019"],
+    ["CSCI005", "CSCI042"],
+    ["PHYS023"],
+    ["CORE099"],
+    ["CHEM024"],
+    ["WRIT001"]
+]
+
+#just an example, not extensive
+HSAReqs = [
+    ["HSA10"],
+    ["WRIT001"],
+    ["HSABreadth"],
+    ["HSADepth"],
+    ["WritInt"]
+]
+
+
+totalReqs = []
+def getTotalReqs(majorReqs, coreReqs, HSAReqs):
+    totalReqs.append(coreReqs)
+    totalReqs.append(majorReqs)
+    totalReqs.append(HSAReqs)
+
+#creating the total reqs
+getTotalReqs(majorReqs, coreReqs, HSAReqs)
+
+def checkReqsMet(inputList):
+    # inputList is a
+    # reqsList is a list of lists
+    for section in totalReqs:
+        for subset in section:
+            met = any(course in subset for course in inputList)
+            if met and not(subset in coursesDone):
+                coursesDone.append(subset)
+            if not(met) and not(subset in coursesToDo):
+                coursesToDo.append(subset)
+coursesToDo = [] # emptied in case we rerun this cell
+coursesDone = [] # emptied in case we rerun this cell
+checkReqsMet(inputCourses)
+
+print("Courses Done is", coursesDone)
+print("Courses ToDo is", coursesToDo) #not pretty, so lets make it pretty
+
+#functions to allow for nice print out
+def printNice(inputList):
+    for i in inputList:
+        print(i)
+
+print("Courses Done:") 
+printNice(coursesDone)
+print("Courses To Do:") 
+printNice(coursesToDo)
+
+
+#this doesn't give us much information about the courses still left to do
+#so lets add a filter to check major/core/HSA etc
+majorReqsDone =[]
+majorReqsToDo = []
+coreReqsDone = []
+coreReqsToDo = []
+HSAReqsDone = []
+HSAReqsToDo = []
+
+def filterReqs(inputList, areaFilter):
+    reqsList = []
+    toDoList = []
+    doneList = []
+    if areaFilter == "Core":
+        reqsList = coreReqs
+        toDoList = coreReqsToDo
+        doneList = coreReqsDone
+    if areaFilter == "Major":
+        reqsList = majorReqs
+        toDoList = majorReqsToDo
+        doneList = majorReqsDone
+    if areaFilter == "HSA":
+        reqsList = HSAReqs
+        toDoList = HSAReqsToDo
+        doneList = HSAReqsDone
+    for subset in reqsList:
+        met = any(course in subset for course in inputList)
+        if met and not(subset in doneList):
+                  doneList.append(subset)
+        if not(met) and not(subset in toDoList):
+                  toDoList.append(subset)
+
+
+#applying the filters
+filterReqs(inputCourses, "HSA")
+filterReqs(inputCourses, "Core")
+filterReqs(inputCourses, "Major")
+
+print("HSA Reqs Met:")
+printNice(HSAReqsDone)
+print("HSA Reqs Not Met:")
+printNice(HSAReqsToDo)
+print("Core Reqs Met:")
+printNice(coreReqsDone)
+print("Core Reqs Not Met:")
+printNice(coreReqsToDo)
+print("Major Reqs Met:")
+printNice(majorReqsDone)
+print("Major Reqs Not Met:")
+printNice(majorReqsToDo)
