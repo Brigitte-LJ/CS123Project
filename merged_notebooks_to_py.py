89b8aec404a983286d414812f7b82ca29f69be84
Nina Luo <niluo@g.hmc.edu>
2024-12-01 13:57:45 -0800

    Completed checkMuddHums, checkprereqs, and relevant helper functions, updated gitignore

--- merged_notebooks_to_py.py
+++ merged_notebooks_to_py.py
@@ -1,5 +1,7 @@
 import csv
 import json
+import mmap
+import re
 
 ### GLOBAL VARIABLES ###
 
@@ -116,47 +118,86 @@ def sample_run_1():
    print("HSA Courses Done is", totalReqsDict["hsaDone"])
    print("HSA Courses ToDo is", totalReqsDict["hsaToDo"])
 
+      
+
+############## TUTORIAL SECTION 2 ################
+
+# Returns a list of all available courses in course-area.json with specified area
+def filterByArea(area):
+  """ TODO: ADD DOCSTRING HERE
+  """
+  with open("course-area.json", 'r') as file: # Open the JSON file
+      course_data = json.load(file)  # Load the JSON data from the file into list
+
+  output = []
+  for course in course_data:  # Print all courses that have the attribute of area
+    if area in set(course["course_areas"]):
+      courseOnly = course["course_code"][:-2].strip() # Remove campus signature & whitespaces
+      output.append(courseOnly) # add course into final list
+  return output # return list of all courses in parameter area.
 
 # Takes in an array of already taken course codes
 # Returns a dictionary containing only the taken course 
 # tagged with HSAs and their corresponding credit value
-# ex) input = ["HIST055  CM", "CSCI140  HM", "ART 005  PO", "DANC051  PO", "ART 060  HM"]
-# output = {'HIST055  CM': '1.0', 'ART 005  PO': '1.0', 'DANC051  PO': '0.5', 'ART 060  HM': 0.5}
-
-def filterHSA(coursesDone):
-    # First, filter out all non-HSAS, we can find out whether or not a course is an HSA by looking in the course-area.json file. 
+#    # ex) input = ["HIST055  CM", "CSCI140  HM", "ART 005  PO", "DANC051  PO", "ART 060  HM", 
+#                   "MUS 175 JM", "MUS 175 JM"]
+#    # output = {'HIST055  CM': '1.0', 'ART 005  PO': '1.0', 'DANC051  PO': '0.5', 
+#                'ART 060  HM': 0.5, 'MUS 175 JM': 1.0}
+# parameter muddHumTrue can be set as True to get an output consisting only of courses tagged 
+# as mudd hums, False otherwise
+def tidyHSA(coursesDone, muddHumTrue):
     with open("course-area.json", 'r') as file: # Open the JSON file containing area data
         courseArea = json.load(file)
     
     with open("course-section.json", 'r') as file: # Open the JSON file containing credit data
         courseSection = json.load(file)
     
-    HSAcredits = {}
-    allTakenData = []
-
+    HSAcredits = {} # initialize empty final returned dictionary
+    allTakenData = [] # initialize empty list storing taken courses & area data immediately below
+    tempList = [] # initialize empty tempList used to store filtered data temporarily
+    
     # Filter through courseArea for taken courses
     # Add taken courses and their corresponding area data into allTakenData
     for course in coursesDone:
         allTakenData.extend(list(filter(lambda x: x["course_code"] == course, courseArea)))
-    
+   
     # Filter through allTakenData
     # Add only courses which are tagged with HSAs into HSAcredits as keys with empty values
-    for taken in allTakenData:
-        if ('4HSS' in taken["course_areas"]) or ('4HSA'in taken["course_areas"]):
-            HSAcredits[taken["course_code"]] = ""
+    if (muddHumTrue): # include only mudd hums with the tag 4HSA
+        for taken in allTakenData:
+            if ('4HSA' in taken["course_areas"]):
+                tempList.append(taken)
+        allTakenData = tempList
+    else: # include only HSAs with either the tag 4HSS or 4HSA or both
+        for taken in allTakenData:
+            if ('4HSS' in taken["course_areas"]) or ('4HSA' in taken["course_areas"]):
+                tempList.append(taken)
+        allTakenData = tempList
 
+    
     # For each HSA taken, find and set its credit amount as the value corresponding to
     # its course code in a key-value pair within the HSAcredits dictionary
-    for HSA in HSAcredits:
+    for taken in allTakenData:
         for course in courseSection:
-            if (HSA in course["courseSectionId"]):
-                HSAcredits[HSA] = course["creditHours"]
+            if (taken["course_code"] in course["courseSectionId"]):
+                if (taken["course_code"] in HSAcredits): # same course taken more than once 
+                    HSAcredits.update({taken["course_code"]: (HSAcredits[taken["course_code"]] 
+                    + float(course["creditHours"]))}) # increment credit amount on the same key
+                else: 
+                    HSAcredits.update({taken["course_code"]: float(course["creditHours"])})
     
     # If the HSA taken is at Mudd, convert its credit value to the 5C credit value
     # i.e. Divide all mudd credits by 3.
     for HSA in HSAcredits:
-        if ("HM" in HSA):
-            HSAcredits[HSA] =  float(HSAcredits[HSA]) / 3.0
+        if ("HM" not in HSA):
+            HSAcredits[HSA] =  float(HSAcredits[HSA]) * 3.0
+    
+    # Special case where Mudd students need 3 semester for one full course of MUS 175 
+    # while other students need only 2 semesters for one full course (This tutorial is 
+    # specified toward mudd students)
+    if ('MUS 175  JM' in HSAcredits):
+        HSAcredits['MUS 175  JM'] = (2 * HSAcredits['MUS 175  JM']) / 3
+        
         
     return HSAcredits
 
@@ -180,7 +221,6 @@ def checkBreadth(niceHSAs):
 
     return (fullCourse >= 5) # There are at least 5 full courses taken in 5 unique areas
 
-
 # Takes in an array of already taken course codes
 # Returns true if depth is fulfilled, false if otherwise.
 # Depth is fulfilled if there are at least 4 full courses taken in one areas. 
@@ -197,30 +237,82 @@ def checkDepth(niceHSAs):
     return (max(uniqueArea.values()) >= 4.0) # there are at least 4.0 credits earned in one area
                                              # (indicating four full courses with the 5C 1.0 scale)
 
+# Creates a global dictionary prereqDict containing all course codes as keys and their 
+# corresponding prereqs as values
+def prereqsToDict():
+    global prerequisite_dict # Define a global variable 
+    prerequisite_dict = {}
+    with open('course.txt', 'r') as f:
+        # memory-map the file
+        mmapped_file = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+        # split the file by |#|, which signify a new course following the symbol |#|
+        courses = mmapped_file.read().decode("utf-8").split("|#|") # Creates a list separated by course
+    
+    courseCodePattern = r"^(.*?)\|\^\|" # use regex to find course code at the beginning of each course string
+    prereqPattern = r"Prerequisite.*" # use regex to find the phrase "prerequisite" in the course description 
+                                # and grab all from "prerequisite" until the end of the string.
+    for course in courses: 
+        matchCode = re.search(courseCodePattern, course)
+        matchPrereq = re.search(prereqPattern, course, re.IGNORECASE)
+        if matchCode and matchPrereq:
+            prerequisite_dict.update({matchCode.group(1): matchPrereq.group(0)}) # stores course code as key, course description as value
 
-#Function calls:
-# done = ["HIST055  CM", "CSCI140  HM", "DANC051  PO", "ANTH190  SC", "ASIA190  PO", "DANC010  PO", "DANC122  PO", "DANC124  PO"]
-# niceHSAs = filterHSA(done)
+# Searches the global dictionary prereqDict to find the parameter courseCode and returns its 
+# corresponding key (prerequisites).
+def checkPrereqFor(courseCode):
+    return prerequisite_dict.get(courseCode)
+
+# Return true if there are at least 4 full Mudd Hums taken(12 HM credits/4 5C credits), 
+# else, return false
+def checkMuddHums(coursesDone):
+    muddHums = tidyHSA(coursesDone, True) # Call tidyHSA with the special parameter 
+                                          # muddHumTrue = True to get a dictionary 
+                                          # of {coursecode:credit} excluding non-mudd hums
+    creditSum = 0
+    
+    for credit in muddHums.values():
+        creditSum += float(credit) # sum up all credits earned from mudd hums
+    
+    print(muddHums) # optional, print all taken mudd hums
+    print(creditSum) # optional, print credit sum of all taken mudd hums so far
+    return (creditSum >= 12) 
+
+# Return true if there are at least 4 full Mudd Hums taken(12 HM credits/4 5C credits), 
+# else, return false
+def checkMuddHums(coursesDone):
+    muddHums = tidyHSA(coursesDone, True) # Call tidyHSA with the special parameter 
+                                          # muddHumTrue = True to get a dictionary 
+                                          # of {coursecode:credit} excluding non-mudd hums
+    creditSum = 0
+    
+    for credit in muddHums.values():
+        creditSum += float(credit) # sum up all credits earned from mudd hums
+    
+    print(muddHums) # optional, print all taken mudd hums
+    print(creditSum) # optional, print credit sum of all taken mudd hums so far
+    return (creditSum >= 12) 
+
+#--# Function calls: #--#
+
+## Define Example List of Taken Courses ##
+# done = ["HIST055  CM", "CSCI140  HM", "DANC051  PO", "ANTH190  SC", "ASIA190  PO", 
+# "DANC010  PO", "DANC122  PO", "DANC124  PO", "AMST179A HM", "ARCT179A HM", "ART 179S HM", 
+# "MUS 175  JM", "MUS 175  JM"]
+
+## Test tidyHSA ##
+# niceHSAs = tidyHSA(done, False) # Returns a list of HSAs and their credits
+# print(niceHSAs)
+
+## Test checkBreadth & checkDepth ##
 # print("Breadth fulfilled:" , checkBreadth(niceHSAs))
 # print("Depth(Concentration) fulfilled:" , checkDepth(niceHSAs))
-      
 
-############## TUTORIAL SECTION 2 ################
+## Test checkMuddHums ##
+# print(checkMuddHums(done)) # Returns a list of Mudd HSAs and their credits
 
-def filterByArea(area):
-  """ TODO: ADD DOCSTRING HERE
-  """
-  with open("course-area.json", 'r') as file: # Open the JSON file
-      course_data = json.load(file)  # Load the JSON data from the file into list
-
-  output = []
-  for course in course_data:  # Print all courses that have the attribute of area
-    if area in set(course["course_areas"]):
-      courseOnly = course["course_code"][:-2].strip() # Remove two-lettered campus signature, remove whitespaces
-      output.append(courseOnly) # add course into final list
-  return output # return list of all courses in parameter area.
-
-
+## Test prereqsToDict & checkPrereqFor ##
+# prereqsToDict()  # only needs to be ran once. (creates a global variable)
+# print(checkPrereqFor("PHYS042 LPO"))
 
 ############## TUTORIAL SECTION 3 ########################
 import ipywidgets as widgets
